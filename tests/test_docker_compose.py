2aa5ab324d1e9cb5a64b3fd0634ab44f1f4206a6
Maxim Karpov <incognito@turbokach.me>
2020-10-21 12:34:21 +0300

    Update test_docker_compose.py
    web теперь обязательно собирать из образа на своем DockerHub

--- tests/test_docker_compose.py
+++ tests/test_docker_compose.py
@@ -11,9 +11,9 @@ class TestDockerfileCompose:
             with open(f'{os.path.join(settings.BASE_DIR, "docker-compose.yaml")}', 'r') as f:
                 docker_compose = f.read()
         except FileNotFoundError:
-            assert False, 'Проверьте, что добавили файл docker_compose.yaml'
+            assert False, 'Проверьте, что добавили файл docker-compose.yaml'
 
         assert re.search(r'image:\s+postgres:', docker_compose), \
-            'Проверьте, что добавили образ postgres:latest в файл docker_compose'
-        assert re.search(r'build:\s+\.', docker_compose), \
-            'Проверьте, что добавили сборку контейнера из Dockerfile в файл docker_compose'
+            'Проверьте, что добавили образ postgres:latest в файл docker-compose.yaml'
+        assert re.search(r'image:\s+([a-zA-Z0-9]+)\/([a-zA-Z0-9_-]+)(\:[a-zA-Z0-9_-]+)', docker_compose), \
+            'Проверьте, что добавили сборку контейнера из образа на вашем DockerHub в файл docker-compose.yaml'
